99bd26d2fe82005e44cfdedfd0d0c3feb8f4c75c
arjunindia <arjun8107@gmail.com>
2023-01-27 23:43:06 +0530

    meme command

--- commands/meme.js
+++ commands/meme.js
@@ -44,7 +44,7 @@ export default defineSlashCommand({
         title: post.title,
         url: `https://reddit.com${post.permalink}`,
         image: {
-          url: post?.preview?.images[0]?.source,
+          url: post?.preview?.images[0]?.source.url,
         },
         footer: {
           text: `Posted by u/${
